4750353c12c66dbc8b082f71ebd2bbd1408d0877
wovert <67668283@qq.com>
2019-04-28 16:15:22 +0800

    style components

---
+++ jianshu/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { GlobalStyled } from './style'
+import Header from './components/header'
+
+function App() {
+  return (
+    <div className="App dell">
+      <GlobalStyled />
+      <Header />
+    </div>
+  );
+}
+
+export default App
